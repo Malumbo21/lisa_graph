6eee712b27ce2155b19ea8c3fb7cccee12fe900f
jack-chipofya-hadat <insedhumasekhame@gmail.com>
2023-12-09 23:07:59 -0800

    dev/feature:added user authentication

--- config/schema.py
+++ config/schema.py
@@ -1,7 +1,12 @@
 import graphene
+import graphql_jwt
 import stock_data.schema
-
-class Query(stock_data.schema.Query, graphene.ObjectType):
+import users.schema
+class Query(users.schema.Query,stock_data.schema.Query, graphene.ObjectType):
 	pass
 
-schema = graphene.Schema(query=Query)
+class Mutation(users.schema.Mutation, graphene.ObjectType):
+	token_auth = graphql_jwt.ObtainJSONWebToken.Field()
+	verify_token = graphql_jwt.Verify.Field()
+	refresh_token = graphql_jwt.Refresh.Field()
+schema = graphene.Schema(query=Query, mutation=Mutation)
