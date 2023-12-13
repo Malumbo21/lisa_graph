03c070607e9940ae81e53d3cfd265164b3870429
jack-chipofya-hadat <insedhumasekhame@gmail.com>
2023-12-13 10:42:44 -0800

    dev/update: Fix query errors

--- stock_data/schema.py
+++ stock_data/schema.py
@@ -12,7 +12,7 @@ class StockDataV2Type(DjangoObjectType):
 		model = StockDataV2
 class Query(graphene.ObjectType):
 	stock_data = graphene.List(StockDataV2Type, start=graphene.String(), end=graphene.String(), symbol=graphene.String(), single_date=graphene.Boolean(), date=graphene.String())
-	top_movers = graphene.List(StockDataV2Type, n=graphene.Int(), day=graphene.Boolean(), week=graphene.Boolean(), month=graphene.Boolean(), year=graphene.Boolean())
+	top_gainers = graphene.List(StockDataV2Type, n=graphene.Int(), day=graphene.Boolean(), week=graphene.Boolean(), month=graphene.Boolean(), year=graphene.Boolean())
 	top_losers = graphene.List(StockDataV2Type, n=graphene.Int(), day=graphene.Boolean(), week=graphene.Boolean(), month=graphene.Boolean(), year=graphene.Boolean())
 	def resolve_stock_data(self, info, start=None, end=None, symbol=None, single_date=False, date=None):
 		data = StockDataV2.objects.all()
@@ -36,59 +36,58 @@ class Query(graphene.ObjectType):
 		return data
 
 	def resolve_top_movers(self, info, n=None, day=False, week=False, month=False, year=False):
-		data = StockDataV2.objects.all().distinct('instrument')
-		data = data.annonate(price_diff = ExpressionWrapper(
-			F('closing_price') - Lag('closing_price', default=F('closing_price'), partition_by=F('instrument')).over(order_by=F('-date')),
-			output_field=fields.DecimalField(),
-			))
-		if [day, week, month, year] == [False, False, False, False]:
-			raise Exception("Error set one on the timeframe options (day, week, month, year) to True")
-		if day == True:
-			date = StockDataV2.objects.aggregate(Max('date'))['date__max']
-			data = StockDataV2.objects.filter(date=date)
-		if week == True:
-			date = StockDataV2.objects.aggregate(Max('date'))['date__max']
-			start_of_week = date - timedelta(days=date.weekday())
-			end_of_week = date + timedelta(days=6)
-			data = StockDataV2.objects.filter(Q(date__range=[start_of_week, end_of_week]) | Q(date__week_day=date.weekday() + 1))
-		if month == True:
-			date = StockDataV2.objects.aggregate(Max('date'))['date__max']
-			data = StockDataV2.objects.filter(Q(date__month=date.month, date__year=date.year))
-		if year == True:
-			date = StockDataV2.objects.aggregate(Max('date'))['date__max']
-			data = StockDataV2.objects.filter(Q(date__year=date.year))
-		data = data.annonate(price_diff = ExpressionWrapper(
-			F('closing_price') - Lag('closing_price', default=F('closing_price'), partition_by=F('instrument')).over(order_by=F('-date')),
-			output_field=fields.DecimalField(),
-			))
+	    if not any([day, week, month, year]):
+	        raise Exception("Error: Set at least one of the timeframe options (day, week, month, year) to True")
 
-		if n is not None:
-			return data.order_by('-price_diff')[:n]
-		else:
-			return data.order_by('-price_diff')[:5]
+	    date = StockDataV2.objects.aggregate(Max('date'))['date__max']
+
+	    if day:
+	        data = StockDataV2.objects.filter(date=date)
+	    elif week:
+	        start_of_week = date - timedelta(days=date.weekday())
+	        end_of_week = start_of_week + timedelta(days=6)
+	        data = StockDataV2.objects.filter(date__range=[start_of_week, end_of_week])
+	    elif month:
+	        data = StockDataV2.objects.filter(date__month=date.month, date__year=date.year)
+	    elif year:
+	        data = StockDataV2.objects.filter(date__year=date.year)
+
+	    data = data.annotate(
+	        price_diff=ExpressionWrapper(
+	            F('closing_price') - Lag('closing_price', default=Value(F('closing_price')), partition_by=F('instrument')).over(order_by=F('-date')),
+	            output_field=fields.DecimalField()
+	        )
+	    )
+
+	    if n is not None:
+	        return data.order_by('-price_diff')[:n]
+	    else:
+	        return data.order_by('-price_diff')[:5]
 	def resolve_top_losers(self, info, n=None, day=False, week=False, month=False, year=False):
-		data = StockDataV2.objects.all().distinct('instrument')
-		data = data.annonate(price_diff = ExpressionWrapper(
-			F('closing_price') - Lag('closing_price', default=F('closing_price'), partition_by=F('instrument')).over(order_by=F('-date')),
-			output_field=fields.DecimalField(),
-			))
-		if [day, week, month, year] == [False, False, False, False]:
-			raise Exception("Error set one on the timeframe options (day, week, month, year) to True")
-		if day == True:
-			date = StockDataV2.objects.aggregate(Max('date'))['date__max']
-			data = StockDataV2.objects.filter(date=date)
-		if week == True:
-			date = StockDataV2.objects.aggregate(Max('date'))['date__max']
-			start_of_week = date - timedelta(days=date.weekday())
-			end_of_week = date + timedelta(days=6)
-			data = StockDataV2.objects.filter(Q(date__range=[start_of_week, end_of_week]) | Q(date__week_day=date.weekday() + 1))
-		if month == True:
-			date = StockDataV2.objects.aggregate(Max('date'))['date__max']
-			data = StockDataV2.objects.filter(Q(date__month=date.month, date__year=date.year))
-		if year == True:
-			date = StockDataV2.objects.aggregate(Max('date'))['date__max']
-			data = StockDataV2.objects.filter(Q(date__year=date.year))
-		if n is not None:
-			return data.order_by('price_diff')[:n]
-		else:
-			return data.order_by('price_diff')[:5]
+	    if not any([day, week, month, year]):
+	        raise Exception("Error: Set at least one of the timeframe options (day, week, month, year) to True")
+
+	    date = StockDataV2.objects.aggregate(Max('date'))['date__max']
+
+	    if day:
+	        data = StockDataV2.objects.filter(date=date)
+	    elif week:
+	        start_of_week = date - timedelta(days=date.weekday())
+	        end_of_week = start_of_week + timedelta(days=6)
+	        data = StockDataV2.objects.filter(date__range=[start_of_week, end_of_week])
+	    elif month:
+	        data = StockDataV2.objects.filter(date__month=date.month, date__year=date.year)
+	    elif year:
+	        data = StockDataV2.objects.filter(date__year=date.year)
+
+	    data = data.annotate(
+	        price_diff=ExpressionWrapper(
+	            F('closing_price') - Lag('closing_price', default=Value(F('closing_price')), partition_by=F('instrument')).over(order_by=F('-date')),
+	            output_field=fields.DecimalField()
+	        )
+	    )
+
+	    if n is not None:
+	        return data.order_by('price_diff')[:n]
+	    else:
+	        return data.order_by('price_diff')[:5]
