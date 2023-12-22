f21751d06206e8ee8c64a71720843e3690e62f69
Suwilanji Chipofya <138398395+suwilanji-chipofya-hadat@users.noreply.github.com>
2023-12-22 15:45:19 +0200

    Update schema.py

--- stock_data/schema.py
+++ stock_data/schema.py
@@ -2,8 +2,8 @@ import graphene
 from datetime import datetime, timedelta
 from stock_data.models import StockDataV2
 from django.db import models
-from django.db.models.functions import Lag, Min, Max, ExpressionWrapper, fields, TruncDate
-from django.db.models import F, Value, Window, ExtractWeek, ExtractMonth, ExtractYear
+from django.db.models.functions import Lag, ExpressionWrapper, fields, TruncDate
+from django.db.models import F, Value, Window, ExtractWeek, ExtractMonth, ExtractYear, Min, Max
 
 from graphene_django import DjangoObjectType
 
